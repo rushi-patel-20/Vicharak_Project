f388e12ccd7a503b8c638eaf4353ae778f511d54
Rushi Patel <160725928+rushi-patel-20@users.noreply.github.com>
2024-08-01 14:48:08 +0530

    Add files via upload

---
+++ src/component/Chat.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import "E:/College/vscode/BootstrapCSS.css";
+
+export default function Chat() {
+  return (
+    <div className="ChatPage">
+      <h1>Chat Module</h1>
+    </div>
+  );
+}
---
+++ src/component/News.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import "E:/College/vscode/BootstrapCSS.css";
+
+export default function News() {
+  return (
+    <div className="NewsPage">
+      <h1>News Module</h1>
+    </div>
+  );
+}
